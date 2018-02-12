0545c7c1610cf776e12217f6c354266892ca64bd
Paudi <padraigloughran@gmail.com>
2018-02-12 11:09:25 +0000

    Setup a connection between library list and the application state (redux & react)

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,11 +4,16 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
   render () {
+    console.log(this.props)
     return;
   }
 }
 
-export default connect()(LibraryList);
+const mapStateToProps = state => {
+  return  { libraries: state.libraries }
+};
+
+export default connect(mapStateToProps)(LibraryList);
 //When connect is called it calls another fucntion, then we call
 //that function with LibraryList (strange call because redux was not only designed for
 // react)
