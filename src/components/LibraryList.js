e627d1a58de0925894f6c5ae65c42c51ce92ec41
Paudi <padraigloughran@gmail.com>
2018-02-12 11:33:01 +0000

    Began implementing a listview, improving performance of overall application when loading data

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
+
+  //Lifecycle method - called when we are about to render this component
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    ths.dataSource = ds.cloneWithRows(thsi.props.libraries); //Use this list of libraries to render content to screen
+  }
+
+  renderRow() {
+
+  }
+
   render () {
-    console.log(this.props)
-    return;
+    return (
+      <ListView dataSource={this.dataSource}
+      renderRow={this.renderRow()} />
+    );
   }
 }
 
